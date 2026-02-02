2e0306741e8d311dbd549df4828e1e275ab06337
meghasaini82 <meghasaini820981@gmail.com>
2026-02-02 20:40:45 +0530

    Update api.js
    Signed-off-by: meghasaini82 <meghasaini820981@gmail.com>

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // Base URL for API
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://product-app-3-5pto.onrender.com/api';
+
 
 // Create axios instance
 const api = axios.create({
